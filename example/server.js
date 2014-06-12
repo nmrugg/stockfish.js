876164badbaf18ad65f6e2eaea86fc24a5f890a4
Nathan Rugg <nmrugg@gmail.com>
2014-06-12 12:08:34 +0800

    Added help if no connect.

--- example/server.js
+++ example/server.js
@@ -1,3 +1,8 @@
-var connect = require('connect');
+try {
+    var connect = require('connect');
+} catch (e) {
+    console.log("Can't find connect. Run 'npm install' from the root.");
+    process.exit();
+}
 connect.createServer(connect.static(__dirname)).listen(8080);
 console.log("Go to http://localhost:8080/");
