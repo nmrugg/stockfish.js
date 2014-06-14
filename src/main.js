13b9489c758e1b82091b75bf9478dd7b10ad0b2b
Nathan Rugg <nmrugg@gmail.com>
2014-06-15 07:59:34 +0800

    Tweaked main.js

--- src/main.js
+++ src/main.js
@@ -15,58 +15,60 @@ The source code for this emscripten port of stockfish can be found
 at http://github.com/exoticorn/stockfish-js
 */
 (function() {
-	if(typeof process == 'undefined') {
-		var initialized = false;
-		
-		onmessage = function(event) {
-			if(!initialized) {
-				Module.ccall('init', 'number', [], []);
-				initialized = true;
-			}
-			if(typeof event.data == 'object') {
-				if(event.data.book) {
-					var book = event.data.book;
-					var byteArray = new Uint8Array(book);
-					var buf = Module._malloc(book.byteLength);
-					Module.HEAPU8.set(byteArray, buf);
-					Module.ccall('set_book', 'number', ['number', 'number'], [buf, book.byteLength]);
-				}
-			} else {
-				Module.ccall('uci_command', 'number', ['string'], [event.data])
-			}
-		}
-		
-		console = {
-			log: function(line) {
-				postMessage(line);
-			}
-		}
-	} else {
-		process.stdin.resume();
-		var lines = null;
-		var init = function() {
-			if(lines === null) {
-				Module.ccall('init', 'number', [], []);
-				lines = '';
-			}			
-		};
-		process.nextTick(init);
-		process.stdin.on('data', function(chunk) {
-			init();
-			lines += chunk;
-			var match;
-			while(match = lines.match(/\r\n|\n\r|\n|\r/)) {
-				var line = lines.slice(0, match.index);
-				lines = lines.slice(match.index + match[0].length);
-				Module.ccall('uci_command', 'number', ['string'], [line]);
-				if(line == 'quit') {
-					process.exit();
-				}
-			}
-		});
-		process.stdin.on('end', function() {
-			process.exit();
-		});
-		
-	}
+    /// Browser
+    if(typeof process == 'undefined') {
+        var initialized = false;
+        
+        onmessage = function(event) {
+            if(!initialized) {
+                Module.ccall('init', 'number', [], []);
+                initialized = true;
+            }
+            if(typeof event.data == 'object') {
+                if(event.data.book) {
+                    var book = event.data.book;
+                    var byteArray = new Uint8Array(book);
+                    var buf = Module._malloc(book.byteLength);
+                    Module.HEAPU8.set(byteArray, buf);
+                    Module.ccall('set_book', 'number', ['number', 'number'], [buf, book.byteLength]);
+                }
+            } else {
+                Module.ccall('uci_command', 'number', ['string'], [event.data])
+            }
+        }
+        
+        console = {
+            log: function(line) {
+                postMessage(line);
+            }
+        }
+    /// Node.js
+    } else {
+        process.stdin.resume();
+        var lines = null;
+        var init = function() {
+            if(lines === null) {
+                Module.ccall('init', 'number', [], []);
+                lines = '';
+            }			
+        };
+        setImmediate(init);
+        process.stdin.on('data', function(chunk) {
+            init();
+            lines += chunk;
+            var match;
+            while(match = lines.match(/\r?\n|\n?\r/)) {
+                var line = lines.slice(0, match.index);
+                lines = lines.slice(match.index + match[0].length);
+                Module.ccall('uci_command', 'number', ['string'], [line]);
+                if(line == 'quit') {
+                    process.exit();
+                }
+            }
+        });
+        process.stdin.on('end', function() {
+            process.exit();
+        });
+        
+    }
 })();
