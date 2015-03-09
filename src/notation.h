f1991fa821f86541324c78e4778badf19d12e369
Nathan Rugg <nmrugg@gmail.com>
2015-03-09 23:35:11 +0800

    Restoring move_to_san()

--- src/notation.h
+++ src/notation.h
@@ -28,6 +28,7 @@ class Position;
 
 std::string score_to_uci(Value v, Value alpha = -VALUE_INFINITE, Value beta = VALUE_INFINITE);
 Move move_from_uci(const Position& pos, std::string& str);
+const std::string move_to_san(Position& pos, Move m);
 const std::string move_to_uci(Move m, bool chess960);
 std::string pretty_pv(const Position& pos, int depth, Value score, int64_t msecs, Move pv[]);
 
