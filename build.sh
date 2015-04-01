64dd007c01a49d849f59536066e7198e54377174
Nathan Rugg <nmrugg@gmail.com>
2015-04-01 23:02:52 +0800

    Preparing to rebuild Stockfish5.

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-cd src
-make build ARCH=js
-cd ..
+set -e
+make -C src build ARCH=js -j `node -e "process.stdout.write(String(require('os').cpus().length))"` && cat src/pre.js src/stockfish.js src/post.js > src/stockfish-make-tmp.js && mv src/stockfish-make-tmp.js src/stockfish.js
