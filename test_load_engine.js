eb8d965193ae2d7b28555d6e540657adc93d7f20
Nathan Rugg <nmrugg@gmail.com>
2015-04-16 00:11:23 +0800

    Support more commands.

--- test_load_engine.js
+++ test_load_engine.js
@@ -1,23 +1,39 @@
 var load_engine = require("./load_engine");
+//var stockfish = load_engine();
 var stockfish = load_engine("stockfish");
 
 stockfish.send("uci");
+    stockfish.send("eval", function ond(d)
+    {
+        console.log(d);
+        stockfish.send("quit");
+    });
 
-stockfish.send("d", function ond(d)
+/*
+stockfish.send("bench", function (str)
 {
-    console.log(d);
+    console.log(str);
     //process.exit();
+    //stockfish.send("uci");
+    
+    stockfish.send("d", function ond(d)
+    {
+        console.log(d);
+        //process.exit();
+    });
+    stockfish.send("go depth 3", function ongo(str)
+    {
+        console.log("found best move: " + str);
+        process.exit();
+    }, function thinking(str)
+    {
+        console.log("thinking: " + str);
+    });
 });
-stockfish.send("go depth 3", function ongo(str)
-{
-    console.log("found best move: " + str);
-    process.exit();
-}, function thinking(str)
-{
-    console.log("thinking: " + str);
-});
+//stockfish.send("uci");
 
 stockfish.stream = function stream_all(data)
 {
-    //console.log("stream: \"" + data + "\"");
+    console.log("stream: \"" + data + "\"");
 }
+*/
