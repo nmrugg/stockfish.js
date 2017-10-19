4d3c921ef0a9b99af615e4d7308b2ce3c2ec5950
Nathan Rugg <nmrugg@gmail.com>
2017-10-19 10:30:48 +0800

    WIP: working on being able to build both versions

--- build.js
+++ build.js
@@ -8,14 +8,20 @@ var params = get_params({booleans: ["no-chesscom", "debug-js", "h", "help", "hel
 var args = ["build", "-j", require("os").cpus().length];
 var fs = require("fs");
 var p = require("path");
-var stockfish_path;
+var stockfishPath = p.join(__dirname, "src", "stockfish");
+var stockfishJSPath = p.join(__dirname, "src", "stockfish.js");
+var stockfishWASMPath = p.join(__dirname, "src", "stockfish.wasm");
+var stockfishWASMLoaderPath = p.join(__dirname, "src", "stockfish.load.js");
 var data;
 var license = fs.readFileSync(p.join(__dirname, "src", "license.js"), "utf8");
-var buildToJs;
+var buildToWASM;
+var buildToASMJS;
+var buildToAnyJS;
 var child;
 var stockfishVersion = "8";
 var postFilePath;
 var postFile;
+var fistRun;
 
 function get_params(options, argv)
 {
@@ -126,18 +132,23 @@ if (!params.make) {
 }
 
 if (params.arch) {
-    args.push("ARCH=" + params.arch);
-    buildToJs = params.arch === "js";
+    if (params.arch === "js") {
+        buildToWASM = true;
+        buildToASMJS = true;
+    } else {
+        args.push("ARCH=" + params.arch);
+        if (params.arch === "asmjs") {
+            buildToASMJS = true;
+        } else if (params.arch === "wasm") {
+            buildToWASM = true;
+        }
+    }
 } else {
-    args.push("ARCH=js");
-    buildToJs = true;
+    buildToWASM = true;
+    buildToASMJS = true;
 }
 
-if (buildToJs) {
-    stockfish_path = p.join(__dirname, "src", "stockfish.js");
-} else {
-    stockfish_path = p.join(__dirname, "src", "stockfish");
-}
+buildToAnyJS = buildToWASM || buildToASMJS;
 
 if (params.help || params["help-all"] || params.h) {
     console.log("");
@@ -146,26 +157,27 @@ if (params.help || params["help-all"] || params.h) {
     console.log("");
     console.log("  " + highlight("-f --force") + "      Always rebuild the entire project");
     console.log("  " + highlight("--force-linking") + " Always preforming the final linking step");
-    console.log("  " + highlight("--variants") + "      Comma separated list of variants to include (default \"" + note("all") + "\")");
-    console.log(                     "                  \"" + note("none") + "\" (no variants, except for Chess960),");
-    console.log(                     "                  \"" + note("anti") + "\", \"" + note("atomic") + "\", \"" + note("crazyhouse") + "\", \"" + note("horde") + "\",");
-    console.log(                     "                  \"" + note("kingofthehill") + "\", \"" + note("race") + "\", \"" + note("relay") + "\", \"" + note("3check") + "\"");
+    console.log("  " + highlight("--variants") + "      Comma separated list of variants to include (default " + note("all") + ")");
+    console.log(                     "                  Possible values are " + note("none") + " (no variants, except for Chess960),");
+    console.log(                     "                  " + note("anti") + ", " + note("atomic") + ", " + note("crazyhouse") + ", " + note("horde") + ", " + note("kingofthehill") + ", " + note("race") + ", " + note("relay") + ", " + note("3check"));
     console.log("  " + highlight("--no-chesscom") + "   Disable changes made specifically for chess.com;");
     console.log(                     "                  this includes showing SAN moves, fixing three-fold repetition,");
-    console.log(                     "                  addition of \"mindepth\", \"maxdepth\", and \"shallow\" options to the \"go\" command,");
-    console.log(                     "                  and \"Skill Level Maximum Error\" and \"Skill Level Probability\" uci options");
+    console.log(                     "                  addition of mindepth, maxdepth, and shallow options to the go command,");
+    console.log(                     "                  and Skill Level Maximum Error and Skill Level Probability uci options");
     console.log("  " + highlight("--sync") + "          Compile Stockfish to run searches synchronously (JS only)");
     console.log("  " + highlight("--debug-js") + "      Compile JS in debug mode (adds ASSERTIONS=2 and SAFE_HEAP=1)");
-    console.log("  " + highlight("--arch") + "          Architecture to build to (default \"" + note("js") + "\")");
-    console.log(                     "                  \"" + note("x86-64-bmi2") + "\" is likely the fastest");
+    console.log("  " + highlight("--arch") + "          Architecture to build to (default " + note("js") + ")");
+    console.log(                     "                  If the arch is set to " + note("js") + ", it will compile both an asm.js version");
+    console.log(                     "                  and a WASM version. Set to " + note("asmjs") + " or " + note("wasm") + " for just one.");
+    console.log(                     "                  " + note("x86-64-bmi2") + " is likely the fastest binary version");
     console.log(                     "                  See --help-all for more options");
-    console.log("  " + highlight("--make") + "          Path to program used to make Stockfish (default \"" + note("make") + "\")");
+    console.log("  " + highlight("--make") + "          Path to program used to make Stockfish (default " + note("make") + ")");
     console.log("  " + highlight("--comp") + "          Compiler to build C code with");
     console.log("  " + highlight("--compcxx") + "       Compiler to build C++ code with");
     console.log("  " + highlight("--version") + "       Specify Stockfish version number (default: " + note(stockfishVersion) + ")");
-    console.log(                     "                  Use \"" + note("date") + "\" to use the current date");
-    console.log(                     "                  Use \"" + note("timestamp") + "\" to use the current Unix timestamp");
-    console.log(                     "                  Use \"" + note("hash") + "\" to use the current git commit hash");
+    console.log(                     "                  Use " + note("date") + " to use the current date");
+    console.log(                     "                  Use " + note("timestamp") + " to use the current Unix timestamp");
+    console.log(                     "                  Use " + note("hash") + " to use the current git commit hash");
     console.log("  " + highlight("-h --help") + "       Show build.js's help");
     console.log("  " + highlight("--help-all") + "      Show Stockfish's Makefile help as well");
     console.log("");
@@ -189,7 +201,16 @@ if (params.help || params["help-all"] || params.h) {
 } else if (params["force-linking"]) {
     ///NOTE: --force will also link as well, so both are not needed.
     try {
-        fs.unlinkSync(stockfish_path);
+        if (buildToAnyJS) {
+            if (buildToASMJS) {
+                fs.unlinkSync(stockfishJSPath);
+            } else {
+                fs.unlinkSync(stockfishJSWASMPath);
+                fs.unlinkSync(stockfishJSWASMLoaderPath);
+            }
+        } else {
+            fs.unlinkSync(stockfishPath);
+        }
     } catch (e) {
         /// Don't throw if there is no file to delete.
         if (e.code !== "ENOENT") {
@@ -207,7 +228,7 @@ if (!params["no-chesscom"]) {
 }
 
 if (params["debug-js"]) {
-    if (buildToJs) {
+    if (buildToAnyJS) {
         args.push("DEBUGJS=1");
     } else {
         warn("Ignoring --debug-js");
@@ -215,7 +236,7 @@ if (params["debug-js"]) {
 }
 
 if (params.sync) {
-    if (buildToJs) {
+    if (buildToAnyJS) {
         args.push("SYNC=1");
         /// Remove code that breaks sync mode
         postFilePath = p.join(__dirname, "src", "post.js");
@@ -246,11 +267,24 @@ if (String(params.version).toLowerCase() !== "date") {
     changeVersion(params.version === true || !params.version ? stockfishVersion : params.version);
 }
 
-child = spawnSync(params.make, args, {stdio: [0,1,2], env: process.env, cwd: p.join(__dirname, "src")});
+
+
+if (buildToWASM && buildToASMJS) {
+    args.push("ARCH=asmjs");
+    args.pop();
+    args.push("ARCH=wasm");
+} else {
+    child = spawnSync(params.make, args, {stdio: [0,1,2], env: process.env, cwd: p.join(__dirname, "src")});
+}
 
 /// Reset version string.
 if (String(params.version).toLowerCase() !== "date") {
     changeVersion("");
+}
+
+if (buildToAnyJS && params.sync) {
+    /// Revert the post file changes from above.
+    fs.writeFileSync(postFilePath, postFile);
 }
 
 /// `make` does not throw an error when encountering errors, so we need to do that manually.
@@ -258,14 +292,20 @@ if (Number(child.status) !== 0) {
     process.exit(Number(child.status));
 }
 
-if (buildToJs) {
-    data = fs.readFileSync(stockfish_path, "utf8");
+if (buildToASMJS) {
+    data = fs.readFileSync(stockfishJSPath, "utf8");
     
     /// Add the license if it's not there (emscripten removes all comments).
     if (data.indexOf(license) !== 0) {
-        fs.writeFileSync(stockfish_path, license + data);
+        fs.writeFileSync(stockfishJSPath, license + data);
     }
-    if (params.sync) {
-        fs.writeFileSync(postFilePath, postFile);
+}
+
+if (buildToWASM) {
+    data = fs.readFileSync(stockfishWASMLoaderPath, "utf8");
+    
+    /// Add the license if it's not there (emscripten removes all comments).
+    if (data.indexOf(license) !== 0) {
+        fs.writeFileSync(stockfishWASMLoaderPath, license + data);
     }
 }
