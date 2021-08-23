72f4a5265ec9a2a4dce53540cb7cfa93c0052fea
Nathan Rugg <nmrugg@gmail.com>
2021-08-23 12:45:06 -0500

    Default to classical mode

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -79,7 +79,7 @@ void init(OptionsMap& o) {
   o["SyzygyProbeDepth"]      << Option(1, 1, 100);
   o["Syzygy50MoveRule"]      << Option(true);
   o["SyzygyProbeLimit"]      << Option(7, 0, 7);
-  o["Use NNUE"]              << Option(true, on_use_NNUE);
+  o["Use NNUE"]              << Option(false, on_use_NNUE);
   o["EvalFile"]              << Option(EvalFileDefaultName, on_eval_file);
 }
 
