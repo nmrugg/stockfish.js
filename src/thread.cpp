0907aa399ea86ec82156fe49df045eadcf6935a0
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-12-05 16:18:33 +0100

    Tweak time management
    Use for adjustment of the falling eval time factor now also the difference
    between previous best average score and current best score.
    
    STC:
    LLR: 2.95 (-2.94,2.94) <0.00,2.50>
    Total: 109216 W: 28296 L: 27900 D: 53020
    Ptnml(0-2): 312, 11759, 30148, 11999, 390
    https://tests.stockfishchess.org/tests/view/61aafa8d1b31b85bcfa29d9c
    
    LTC:
    LLR: 2.93 (-2.94,2.94) <0.50,3.00>
    Total: 54096 W: 14091 L: 13787 D: 26218
    Ptnml(0-2): 29, 5124, 16447, 5410, 38
    https://tests.stockfishchess.org/tests/view/61abbbbd56fcf33bce7d1d64
    
    closes https://github.com/official-stockfish/Stockfish/pull/3833
    
    Bench: 4829419

--- src/thread.cpp
+++ src/thread.cpp
@@ -164,6 +164,7 @@ void ThreadPool::clear() {
 
   main()->callsCnt = 0;
   main()->bestPreviousScore = VALUE_INFINITE;
+  main()->bestPreviousAverageScore = VALUE_INFINITE;
   main()->previousTimeReduction = 1.0;
 }
 
