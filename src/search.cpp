4506beeba8ae1138f167f3a6e2a9a3e5507abeb4
bmc4 <bmc4@cin.ufpe.br>
2021-12-17 21:14:34 -0300

    Remove Capture Extension
    This revert the patch #3692, probably can be simplified after the introduction of #3838.
    
    Fixed-game test:
    ELO: -1.41 +-1.8 (95%) LOS: 5.9%
    Total: 20000 W: 1552 L: 1633 D: 16815
    Ptnml(0-2): 38, 1242, 7517, 1169, 34
    https://tests.stockfishchess.org/tests/view/61bc1a2057a0d0f327c32a3c
    
    STC:
    LLR: 2.94 (-2.94,2.94) <-2.25,0.25>
    Total: 44528 W: 11619 L: 11478 D: 21431
    Ptnml(0-2): 146, 5020, 11771, 5201, 126
    https://tests.stockfishchess.org/tests/view/61bc638c57a0d0f327c338fe
    
    LTC:
    LLR: 2.94 (-2.94,2.94) <-2.25,0.25>
    Total: 34136 W: 8847 L: 8704 D: 16585
    Ptnml(0-2): 23, 3475, 9925, 3626, 19
    https://tests.stockfishchess.org/tests/view/61bcb24257a0d0f327c34813
    
    closes https://github.com/official-stockfish/Stockfish/pull/3863
    
    Bench: 4054695

--- src/search.cpp
+++ src/search.cpp
@@ -1131,12 +1131,6 @@ moves_loop: // When in check, search starts here
               extension = -2;
       }
 
-      // Capture extensions for PvNodes and cutNodes
-      else if (   (PvNode || cutNode)
-               && captureOrPromotion
-               && moveCount != 1)
-          extension = 1;
-
       // Check extensions
       else if (   givesCheck
                && depth > 6
