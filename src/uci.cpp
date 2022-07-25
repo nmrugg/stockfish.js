493d9ffd2a186625e2852de576791f91dced433e
Nathan Rugg <nmrugg@gmail.com>
2022-07-25 11:09:32 -0500

    Add command line flags to link to information
    This patch provides command line flags `--help` and `--license` as well as the corresponding `help` and `license` commands.
    
    ```
    $ ./stockfish --help
    Stockfish 200522 by the Stockfish developers (see AUTHORS file)
    
    Stockfish is a powerful chess engine and free software licensed under the GNU GPLv3.
    Stockfish is normally used with a separate graphical user interface (GUI).
    Stockfish implements the universal chess interface (UCI) to exchange information.
    For further information see https://github.com/official-stockfish/Stockfish#readme
    or the corresponding README.md and Copying.txt files distributed with this program.
    
    ```
    
    The idea is to provide a minimal help that links to the README.md file,
    not replicating information that is already available elsewhere.
    
    We use this opportunity to explicitly report the license as well.
    
    closes https://github.com/official-stockfish/Stockfish/pull/4027
    
    No functional change.
    
    # Conflicts:
    #	src/uci.cpp

--- src/uci.cpp
+++ src/uci.cpp
@@ -316,8 +316,14 @@ void UCI::loop(int argc, char* argv[]) {
       #ifdef __EMSCRIPTEN__
       else if (token == "bench_eval") bench_eval(pos);
       #endif
+      else if (token == "--help" || token == "help" || token == "--license" || token == "license")
+          sync_cout << "\nStockfish is a powerful chess engine and free software licensed under the GNU GPLv3."
+                       "\nStockfish is normally used with a separate graphical user interface (GUI)."
+                       "\nStockfish implements the universal chess interface (UCI) to exchange information."
+                       "\nFor further information see https://github.com/official-stockfish/Stockfish#readme"
+                       "\nor the corresponding README.md and Copying.txt files distributed with this program.\n" << sync_endl;
       else if (!token.empty() && token[0] != '#')
-          sync_cout << "Unknown command: " << cmd << sync_endl;
+          sync_cout << "Unknown command: '" << cmd << "'. Type help for more information." << sync_endl;
 
   } while (token != "quit" && argc == 1); // Command line args are one-shot
 
