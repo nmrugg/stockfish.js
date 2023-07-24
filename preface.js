1c68365b91c7f3bfa379f6de1cc6b776302198ef
Nathan Rugg <nmrugg@gmail.com>
2023-07-24 16:58:12 -0500

    removed more old files

--- preface.js
+++
@@ -1,9 +0,0 @@
-/*!
- * Stockfish.js (http://github.com/nmrugg/stockfish.js)
- * License: GPL
- */
-var Stockfish;
-function INIT_ENGINE()
-{
-var STOCKFISH = function (console, wasmPath, isInitializer)
-{
